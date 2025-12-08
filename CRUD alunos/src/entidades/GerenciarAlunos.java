895130ba24edaa5810ce8e194fe24362f10d9e6b
Jonathan Cavalcante <jonathancavalcante1911@gmail.com>
2025-12-08 16:45:11 -0300

    .

---
+++ CRUD alunos/src/entidades/GerenciarAlunos.java
@@ -0,0 +1,10 @@
+package entidades;
+import java.util.ArrayList;
+public class GerenciarAlunos {
+    private ArrayList<Alunos> alunos = new ArrayList<>();
+
+    public void adicionarAluno(Alunos aluno) {
+        alunos.add(aluno);
+    }
+
+}
